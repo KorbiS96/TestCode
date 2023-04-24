a506a0d7eeb3ab66ee79f551b8a89886eb7a2804
KorbiS96 <126156705+KorbiS96@users.noreply.github.com>
2023-04-24 11:26:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from source.components.layout import create_layout
 import dash_bootstrap_components as dbc
 
 # stylesheet with the .dbc class
-server = app.server
+
 
 def main () -> None:
     app = Dash()
+    server = app.server
     app.title = "GFCC Database"
     app.layout = create_layout(app)
     app.run()
